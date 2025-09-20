e5f22e6e7c5df9b2ba1ceee9777f63ffd5915e78
NishantKhorwal <khorwalnishant@gmail.com>
2025-09-20 17:52:56 +0530

    solving cors problem

--- backend/api/index.js
+++ backend/api/index.js
@@ -26,7 +26,8 @@ const server = createServer(app);
 // CORS Configuration (Must be on Top)
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:5174"
+  "http://localhost:5174",
+  "https://rofinfra.in"
 ];
 
 app.use(cors({
